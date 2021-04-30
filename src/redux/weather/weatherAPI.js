3d0457f0fbb9b43ab341f949573474fe9cc1a488
Matthew Piedra <matthewpiedra1228@gmail.com>
2021-04-29 22:12:16 -0400

    fix env variable

--- src/redux/weather/weatherAPI.js
+++ src/redux/weather/weatherAPI.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const url = `https://api.openweathermap.org/data/2.5`;
+const key = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_KEY : window.env.API_KEY;
 
 async function fetchWeather(city) {
     try {
-        const res = await axios.get(`${url}/weather?q=${city}&units=imperial&appid=${window.env.API_KEY}`);
+        const res = await axios.get(`${url}/weather?q=${city}&units=imperial&appid=${key}`);
         return res;
     }
     catch(err) {
